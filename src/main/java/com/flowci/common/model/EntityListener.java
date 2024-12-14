8b4042290f14bf063e892cce4dce8d9e59452a14
gy2006 <32008001@qq.com>
2024-12-14 23:19:52 +0100

    setup github action

--- src/main/java/com/flowci/common/model/EntityListener.java
+++ src/main/java/com/flowci/common/model/EntityListener.java
@@ -4,21 +4,22 @@ import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 public class EntityListener {
 
     @PrePersist
     public void prePersist(Object entity) {
         if (entity instanceof EntityBase e) {
-            e.createdAt = Instant.now();
-            e.updatedAt = Instant.now();
+            e.createdAt = Instant.now().truncatedTo(ChronoUnit.MICROS);
+            e.updatedAt = Instant.now().truncatedTo(ChronoUnit.MICROS);
         }
     }
 
     @PreUpdate
     public void preUpdate(Object entity) {
         if (entity instanceof EntityBase e) {
-            e.updatedAt = Instant.now();
+            e.updatedAt = Instant.now().truncatedTo(ChronoUnit.MICROS);
         }
     }
 }
